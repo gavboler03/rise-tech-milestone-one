3f9612223b87b52cba2ad8d1b94b21dba2a3ba50
gavboler03 <89207920+gavboler03@users.noreply.github.com>
2026-06-27 17:39:14 -0500

    national view only user cannot see approval queue

--- tests/natuser.spec.ts
+++ tests/natuser.spec.ts
@@ -11,14 +11,35 @@ const testNationalViewOnlyPassword = String(
   process.env.TEST_NATIONAL_VIEWONLY_PASSWORD,
 );
 
-const users = [
-  {
-    username: testNationalViewOnlyUsername,
-    password: testNationalViewOnlyPassword,
-    role: "National User",
-  },
-];
-
-test(`National user does not see Approval Queue dashboard widget or tab`, async ({
+const user = {
+  username: testNationalViewOnlyUsername,
+  password: testNationalViewOnlyPassword,
+  role: "National User",
+};
+test(`National user can log in and does not see Approval Queue dashboard widget or tab`, async ({
   page,
-}) => {});
+}) => {
+  // Login as National User
+
+  await page.goto("https://member.fop.net/signin/");
+
+  await expect(page).toHaveTitle("");
+
+  await page.locator("#username").fill(testNationalViewOnlyUsername);
+  await page.locator("#password").fill(testNationalViewOnlyPassword);
+
+  await page.waitForTimeout(500);
+
+  const button = page.getByRole("button", { name: /sign in|log in/i });
+
+  await expect(button).toBeVisible();
+  await expect(button).toBeEnabled();
+
+  await Promise.all([page.waitForURL(/dashboard/), button.click()]);
+
+  await expect(page.getByText(user.role)).toBeVisible();
+
+  // Make sure Approval Queue page is not visible
+
+  await expect(page.getByText("Approval Queue")).not.toBeVisible();
+});
